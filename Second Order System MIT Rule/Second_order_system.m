81fc8d6d338bdd98f1a6f2d4aa81be476477ab10
msezer <mustafacanersezer@gmail.com>
2020-04-05 19:24:05 +0300

    first commit

---
+++ Second Order System MIT Rule/Second_order_system.m
@@ -0,0 +1,11 @@
+close all; clear all; clc;
+%% Process 
+b = 7; %The constant was chosen arbitrarily
+B = b;
+A = [1 7];
+%% Model
+Bm = 12;
+Am = [1 7 12];
+%% Controller,
+gamma = 1.2;
+alpha = 10;
